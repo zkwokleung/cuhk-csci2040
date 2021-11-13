d0263d92b24f46b8542bd3e2a22a203fabd3211b
Andrew SZETO <zkwokleung@gmail.com>
2021-11-14 02:22:09 +0800

    UPDATE: complete p1.py

--- labs/lab4/p1.py
+++ labs/lab4/p1.py
@@ -27,8 +27,7 @@ fruit = [
 print([k.upper() for (k, v) in vehicle_dict.items() if vehicle_dict[k] < 2500])
 
 # 2.
-print([' '.join([f"{j}*{i}={j*i}" for j in range(1, i+1)])
-      for i in range(1, 10)])
+print([' '.join([f"{j}*{i}={j*i}" for j in range(1, i+1)]) for i in range(1, 10)])
 
 # 3.
 print(list(map(lambda x: x*x, range(1, 11))))
@@ -37,4 +36,4 @@ print(list(map(lambda x: x*x, range(1, 11))))
 print(list(filter(lambda x: x % 2 == 0, range(1, 11))))
 
 # 5.
-# TODO
+print({k: reduce(lambda a, b: (a+b) * 1.0, [e[k] for e in fruit]) for k in fruit[0].keys()})
