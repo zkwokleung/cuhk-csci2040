0c008f8f07dd9bc7defb09c5802632c672611e6d
Andrew SZETO <zkwokleung@gmail.com>
2021-11-16 00:35:15 +0800

    UPDATE: p4.py more pythonic

--- labs/lab4/p4.py
+++ labs/lab4/p4.py
@@ -6,6 +6,4 @@ def get_final_grades(filename='grades.csv'):
     weights = [0.2, 0.25, 0.25, 0.3]
     with open(filename) as file:
         # Almost unreadable but pythonic one line code of calculating the students grades
-        students_grades = {r[0]: reduce(lambda a, b: a+b, [float(r[i+1]) * weights[i] if float(
-            r[i+1]) != -1 else 0 for i in range(4)]) for r in csv.reader(file)}
-    return students_grades
+        return {r[0]: reduce(lambda a, b: a+b, [float(r[i+1]) * weights[i] if float(r[i+1]) != -1 else 0 for i in range(4)]) for r in csv.reader(file)}
