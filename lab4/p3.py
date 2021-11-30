2d585315192daa58779fd6d2e8f648cfa39017d5
Andrew SZETO <zkwokleung@gmail.com>
2021-12-01 00:31:49 +0800

    UPDATE: comments for submission

--- lab4/p3.py
+++ lab4/p3.py
@@ -1,12 +1,9 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
 import matplotlib.pyplot as plt
 import matplotlib.pyplot as plt2
 import matplotlib.pyplot as plt3
 import numpy as np
+
+
 def ex3():
     np.random.seed(0)
     mu = 0
@@ -17,11 +14,12 @@ def ex3():
                                 density=1,
                                 color='green')
     x = bins
-    y = np.exp((-np.power(x-mu, 2)/np.power(2*sigma, 2))/(sigma*np.sqrt(2*np.pi)))
+    y = np.exp((-np.power(x-mu, 2)/np.power(2*sigma, 2)) /
+               (sigma*np.sqrt(2*np.pi)))
 
-    #Q1
-    plt.plot(x, y, color = 'red',
-                linestyle = 'dashed')
+    # Histogram and line chart
+    plt.plot(x, y, color='red',
+             linestyle='dashed')
     plt.savefig('histogram_line.png', dpi=100)
     plt.show()
 
@@ -34,18 +32,18 @@ def ex3():
         ppls.append(y)
     explode = (0, 0, 0, 0.1)
 
-    #Q2
+    # Pie chart
     plt2.pie(ppls, explode=explode, labels=names,
-            autopct='%1.1f%%', startangle=0,
-            wedgeprops={"edgecolor": "black",
-                        'linewidth': 2,
-                        'antialiased': True})
-    plt2.savefig('pie.png', dpi = 100)
+             autopct='%1.1f%%', startangle=0,
+             wedgeprops={"edgecolor": "black",
+                         'linewidth': 2,
+                         'antialiased': True})
+    plt2.savefig('pie.png', dpi=100)
     plt2.show()
 
-    #Q3
-    plt3.bar(names, ppls, color ='maroon',
-        width = 0.4)
+    # Bar chart
+    plt3.bar(names, ppls, color='maroon',
+             width=0.4)
 
     plt3.savefig('bar.png', dpi=100)
     plt3.show()
