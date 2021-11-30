8879dd02a1672b69f4de4f4d69eee8bf67ace6f0
TangTang <ggwp12336@gmail.com>
2021-11-30 13:45:54 +0800

    Update: Q3_not yet check
    Q3

--- lab4/p3.py
+++ lab4/p3.py
@@ -1,17 +1,55 @@
-import matplotlib
-import numpy as np
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
 import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt2
+import matplotlib.pyplot as plt3
+import numpy as np
+def ex3():
+    np.random.seed(0)
+    mu = 0
+    sigma = 1
+    random_numbers = mu + sigma*np.random.randn(10000)
+    num_bins = 100
+    n, bins, patches = plt.hist(random_numbers, num_bins,
+                                density=1,
+                                color='green')
+    x = bins
+    y = np.exp((-np.power(x-mu, 2)/np.power(2*sigma, 2))/(sigma*np.sqrt(2*np.pi)))
 
-np.random.seed(0)
-mu = 0
-sigma = 1
-x = mu + sigma * np.random.randn(10000)
+    #Q1
+    plt.plot(x, y, color = 'red',
+                linestyle = 'dashed')
+    plt.savefig('histogram_line.png', dpi=100)
+    plt.show()
 
-# histogram and line chart here 
+    Colleges = {'New Asia College': 3345, 'United College': 3364,
+                'Shaw College': 3342, 'Morningside College': 300}
+    names = []
+    ppls = []
+    for x, y in Colleges.items():
+        names.append(x)
+        ppls.append(y)
+    explode = (0, 0, 0, 0.1)
 
-Colleges = {'New Asia College': 3345, 'United College': 3364,
-'Shaw College': 3342, 'Morningside College': 300}
+    #Q2
+    plt2.pie(ppls, explode=explode, labels=names,
+            autopct='%1.1f%%', startangle=0,
+            wedgeprops={"edgecolor": "black",
+                        'linewidth': 2,
+                        'antialiased': True})
+    plt2.savefig('pie.png', dpi = 100)
+    plt2.show()
 
-# pie chart
+    #Q3
+    plt3.bar(names, ppls, color ='maroon',
+        width = 0.4)
 
-# bar chart
+    plt3.savefig('bar.png', dpi=100)
+    plt3.show()
+
+
+if __name__ == '__main__':
+    ex3()
