65920f648879a87431608cc3a349f2709c17a027
Andrew SZETO <zkwokleung@gmail.com>
2021-12-02 02:26:16 +0800

    UPDATE: new approach

--- lab5/lab5_2021/package/plot.py
+++ lab5/lab5_2021/package/plot.py
@@ -2,47 +2,44 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import numpy as np
 import random
-from sortTime import selectSort
 import myModule
 
 
-# The array size
-arr_size = 10000
+arr_size = 10000  # The size of array and the x limit
+low_bound, up_bound = -1000000, 1000000  # The bound of random numbers
 
 # Two excatly same list for 2 sorting method
-lst4python = [random.randint(-1000000, 1000000) for _ in range(arr_size)]
+lst4python = [random.randint(low_bound, up_bound) for _ in range(arr_size)]
 lst4C = lst4python.copy()
 x = np.arange(0, arr_size)
 running = False
 
 # The plot
 fig, ax = plt.subplots(nrows=2)
-ax[0].scatter(x, lst4python, s=2, c="red")
 
 # Python
 ax[0].set_title("Sort using Python")
+# Hide the X axis since it is overlapping with the graph below
 ax[0].get_xaxis().set_visible(False)
+ln_python, = ax[0].plot(x, lst4python, "ro")
 
 # C
 ax[1].set_title("Sort using C")
-ax[1].scatter(x, lst4C, s=2, c="blue")
+ln_C, = ax[1].plot(x, lst4C, "bo")
 
 # Animation function
 
 
 def init():
     ax.set_xlim(0, arr_size)
-    ax.set_ylim(-1000000, 1000000)
+    ax.set_ylim(low_bound, up_bound)
 
 
 def update(frame):
-    global running
-    if not running:
-        selectSort(lst4python)
-        running = True
+    pass
 
 
-ani = FuncAnimation(fig, update, frames=np.linspace(0, 2*np.pi, 128),
+ani = FuncAnimation(fig, update, frames=np.linspace(0, arr_size, arr_size),
                     init_func=init, blit=True)
 
 plt.show()
