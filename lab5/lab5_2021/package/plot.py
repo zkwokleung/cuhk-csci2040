3178089a6d1026a241969ecf888402611c8c10f0
Andrew SZETO <zkwokleung@gmail.com>
2021-12-02 04:07:14 +0800

    UPDATE: EX2

--- lab5/lab5_2021/package/plot.py
+++ lab5/lab5_2021/package/plot.py
@@ -2,17 +2,40 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import numpy as np
 import random
-import myModule
 
 
+# Controlable variables
+time_python = 10  # Time taken to perform the sort in python
+time_C = 1  # Time taken to perform the sort in C
 arr_size = 10000  # The size of array and the x limit
 low_bound, up_bound = -1000000, 1000000  # The bound of random numbers
 
-# Two excatly same list for 2 sorting method
-lst4python = [random.randint(low_bound, up_bound) for _ in range(arr_size)]
-lst4C = lst4python.copy()
-x = np.arange(0, arr_size)
-running = False
+# Runtime variables
+steps = []  # An array to store the intermediate steps
+ratio = time_python / time_C  # the ratio
+origin_lst = [random.randint(low_bound, up_bound)
+              for _ in range(arr_size)]  # list for sorting
+x = np.arange(0, arr_size)  # The x value for the plot
+
+
+# Modified selectSort to store every intermediate step to a list
+def selectSort(list):
+    for i in range(len(list)):
+        min_idx = i
+        for j in range(i+1, len(list)):
+            if list[j] < list[min_idx]:
+                min_idx = j
+        list[i], list[min_idx] = list[min_idx], list[i]
+        steps.append(list[::])
+
+
+# get the intermediate steps
+selectSort(origin_lst)
+steps = steps[::10]  # Skipping some steps to speed up the animation
+lst4python = steps[::]  # The y value for the Python plot
+# The y value for the C plot. Skipping some steps to visualize the speed differences
+lst4C = steps[::int(ratio)]
+
 
 # The plot
 fig, ax = plt.subplots(nrows=2)
@@ -21,25 +44,33 @@ fig, ax = plt.subplots(nrows=2)
 ax[0].set_title("Sort using Python")
 # Hide the X axis since it is overlapping with the graph below
 ax[0].get_xaxis().set_visible(False)
-ln_python, = ax[0].plot(x, lst4python, "ro")
+ln_python, = ax[0].plot(x, lst4python[0], "ro")
 
 # C
 ax[1].set_title("Sort using C")
-ln_C, = ax[1].plot(x, lst4C, "bo")
+ln_C, = ax[1].plot(x, lst4C[0], "bo")
+
 
 # Animation function
-
-
 def init():
-    ax.set_xlim(0, arr_size)
-    ax.set_ylim(low_bound, up_bound)
+    ax[0].set_xlim(0, arr_size)
+    ax[0].set_ylim(low_bound, up_bound)
+    ax[1].set_xlim(0, arr_size)
+    ax[1].set_ylim(low_bound, up_bound)
+    return [ln_python, ln_C]
 
 
 def update(frame):
-    pass
+    ln_python.set_data(x, lst4python[frame])
+    if frame >= len(lst4C):
+        # Display the last frame for the c plot
+        ln_C.set_data(x, lst4C[-1])
+    else:
+        ln_C.set_data(x, lst4C[frame])
+    return [ln_python, ln_C]
 
 
-ani = FuncAnimation(fig, update, frames=np.linspace(0, arr_size, arr_size),
+ani = FuncAnimation(fig, update, frames=range(len(steps)),
                     init_func=init, blit=True)
 
 plt.show()
