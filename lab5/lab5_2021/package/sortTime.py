2951fec0d36becb8159b99c51f8ec5e4da9d1c88
Andrew SZETO <zkwokleung@gmail.com>
2021-12-01 22:17:57 +0800

    UPDATE: sortTime function for importation

--- lab5/lab5_2021/package/sortTime.py
+++ lab5/lab5_2021/package/sortTime.py
@@ -16,26 +16,27 @@ def selectSort(list):
     return list
 
 
-# User Input
-flag = True
-while flag:
-    arr_size = int(input("Input array size: "))
-    if 1 <= arr_size <= 20000:
-        flag = False
-    else:
-        print("Size must be within 1 to 20000.")
+if __name__ == "__main__":
+    # User Input
+    flag = True
+    while flag:
+        arr_size = int(input("Input array size: "))
+        if 1 <= arr_size <= 20000:
+            flag = False
+        else:
+            print("Size must be within 1 to 20000.")
 
-# Generate a random list
-lst = [random.randint(-1000000, 1000000) for _ in range(arr_size)]
+    # Generate a random list
+    lst = [random.randint(-1000000, 1000000) for _ in range(arr_size)]
 
-# Test the runtime in C
-start = time.time()
-myModule.selectSort(lst)
-end = time.time()
-print("Time used in C extension selection sort {:.3f}".format(end - start))
+    # Test the runtime in C
+    start = time.time()
+    myModule.selectSort(lst)
+    end = time.time()
+    print("Time used in C extension selection sort {:.3f}".format(end - start))
 
-# Test the runtime in python
-start = time.time()
-selectSort(lst)
-end = time.time()
-print("Time used in Python selection sort {:.3f}".format(end - start))
+    # Test the runtime in python
+    start = time.time()
+    selectSort(lst)
+    end = time.time()
+    print("Time used in Python selection sort {:.3f}".format(end - start))
